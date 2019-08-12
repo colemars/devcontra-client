93fa6385a427dac57982d53c89ac1743594c4ed2
Cole Marsteller <cgm32547@gmail.com>
2019-08-11 17:40:29 -0700

    add signup page

---
+++ pages/signup.js
@@ -0,0 +1,13 @@
+import React, { useState, useContext } from 'react';
+import Layout from '../components/Layout';
+
+const SignUp = () => {
+
+  return (
+    <Layout>
+      
+    </Layout>
+  )
+}
+
+export default SignUp;
