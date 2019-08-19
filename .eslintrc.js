0f489ebae2be9c650bf8c031b836ef9d188c7139
Cole Marsteller <cgm32547@gmail.com>
2019-08-19 15:59:02 -0700

    get eslint working

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,44 +1,44 @@
 module.exports = {
-    env: {
-      es6: true,
-      browser: true
-    },
-    extends: [
-      "airbnb",
-      "prettier",
-      "prettier/react",
-      "plugin:react/recommended",
-    ],
-    globals: {
-      Atomics: 'readonly',
-      SharedArrayBuffer: 'readonly',
-    },
-    parserOptions: {
-      ecmaVersion: 2018,
-      ecmaFeatures: {
-        experimentalObjectRestSpread: true,
-        jsx: true
-    },
-      sourceType: 'module',
-    },
-    rules: {
-      "prettier/prettier": "error",
-      "react/react-in-jsx-scope": "off",
-      "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
-      "jsx-a11y/anchor-is-valid": [ "error", {
-        "components": [ "Link" ],
-        "specialLink": [ "hrefLeft", "hrefRight" ],
-        "aspects": [ "invalidHref", "preferButton" ]
-      }]
-    },
-    plugins: [
-      "prettier", "react"
-    ],
-    settings: {
-      react: {
-        pragma: "React",
-        version: "detect"
-      }
-    },
-  };
-  
+  env: {
+    es6: true,
+    browser: true
+  },
+  extends: [
+    "airbnb",
+    "prettier",
+    "prettier/react",
+    "plugin:react/recommended",
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+      jsx: true
+  },
+    sourceType: 'module',
+  },
+  rules: {
+    "prettier/prettier": "error",
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+    "jsx-a11y/anchor-is-valid": [ "error", {
+      "components": [ "Link" ],
+      "specialLink": [ "hrefLeft", "hrefRight" ],
+      "aspects": [ "invalidHref", "preferButton" ]
+    }]
+  },
+  plugins: [
+    "prettier", "react"
+  ],
+  settings: {
+    react: {
+      pragma: "React",
+      version: "detect"
+    }
+  },
+  "parser": "babel-eslint"
+};
