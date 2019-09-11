459cf4ae65aa360d3c6fad413bb97fe83537b517
Cole Marsteller <cgm32547@gmail.com>
2019-09-10 17:56:49 -0700

    Add Data context

--- context/user-context.js
+++ context/user-context.js
@@ -6,5 +6,14 @@ const useAuth = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   return { isLoggedIn, setIsLoggedIn };
 };
+// re-render consumers only when value.isLoggedIn changes
+export const useAuthContext = createUseContext(useAuth, value => [
+  value.isLoggedIn,
+]);
 
-export const useAuthContext = createUseContext(useAuth);
+const useData = () => {
+  const [data, setData] = useState([]);
+  return { data, setData };
+};
+
+export const useDataContext = createUseContext(useData, value => [value.data]);
