73bed52c92c3ade3df5d68442115cc9bf20ef146
Cole Marsteller <cgm32547@gmail.com>
2019-09-23 22:21:15 -0700

    display timeline on Demo page

--- pages/demo.js
+++ pages/demo.js
@@ -1,15 +1,7 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
-import Paper from '@material-ui/core/Paper';
-import { useRouter } from 'next/router';
-import v4 from 'uuid';
-import clsx from 'clsx';
-// eslint-disable-next-line import/no-extraneous-dependencies
 import DevContra from 'devcontra-component';
-import stackOverflow from '../dummy';
-import { useAuthContext, useDataContext } from '../context/user-context';
 
 const useStyles = makeStyles(theme => ({
   container: {
@@ -53,64 +45,23 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-const Configure = () => {
-  // const router = useRouter();
+const Demo = () => {
   const classes = useStyles();
-  // const { isLoggedIn } = useAuthContext();
-  // const { data } = useDataContext();
-  const url = { stackoverflow: '/static/stack.png' };
-
-  // console.log(stackOverflow);
-
-  // useEffect(() => {
-  //   if (!isLoggedIn) {
-  //     router.replace('/signin ');
-  //   }
-  // }, [isLoggedIn]);
 
   return (
     <div className={classes.root}>
+      <div style={{ borderColor: 'red', borderWidth: 1 }}>
+        <DevContra email={process.env.EMAIL} password={process.env.PASSWORD} />
+      </div>
       <Box display={{ xs: 'block', md: 'none' }}>
         hide on screens wider than md
       </Box>
       {/* hide on screens smaller than md */}
       <Box display={{ xs: 'none', md: 'block' }}>
-        <Grid className={classes.container} container spacing={0}>
-          {stackOverflow.map(post => (
-            <Grid item xs={3} key={v4()}>
-              <div
-                className={clsx(classes[post.variant], classes.paper, 'card')}
-                key={post.postId}
-              >
-                <div className={classes.logo}>
-                  <img
-                    src={url[post.variant]}
-                    className={classes.markerIcon}
-                    alt={`${post.variant} logo`}
-                  />
-                </div>
-              </div>
-              <style jsx>{`
-                .card {
-                  cursor: pointer;
-                }
-                .card:hover {
-                  opacity: 0.5;
-                }
-              `}</style>
-            </Grid>
-          ))}
-        </Grid>
-        <DevContra email="EMAIL" password="PASSWORD" />
-        {/* <Grid container>
-          <Grid item xs={4} />
-          <Grid item xs={4} container alignItems="center" justify="center">
-          </Grid>
-          <Grid item xs={4} />
-        </Grid> */}
+        <div>{process.env.TEST}</div>
       </Box>
     </div>
   );
 };
 
-export default Configure;
+export default Demo;
